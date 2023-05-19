af86b80a6d9b4ae635cc647106dbd6ec22af3a4c
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2023-05-19 19:16:52 -0400

    all functions

--- main.py
+++ main.py
@@ -10,8 +10,25 @@ screen.listen()
 
 # Forwards function
 def move_forward():
-    cursor.foward(10)
+    cursor.forward(5)
 
 # Backwards function
-def move_backwards():
-    cursor.backward(10)
+def move_backward():
+    cursor.backward(5)
+
+# Clockwise-turn function
+def turn_clockwise():
+    cursor.setheading(cursor.heading() + 5)
+
+# Counter-clockwise function
+def turn_counterclockwise():
+    cursor.setheading(cursor.heading() - 5)
+
+# Keypress functions
+screen.onkeypress(move_forward, "w")
+screen.onkeypress(move_backward, "s")
+screen.onkeypress(turn_clockwise, "d")
+screen.onkeypress(turn_counterclockwise, "a")
+
+# Make screen exit on click 
+screen.exitonclick()
