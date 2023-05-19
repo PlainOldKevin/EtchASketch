440211dd75af5156252c01ce974707ce246ebeef
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2023-05-19 18:55:20 -0400

    forwards and backwards functions

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ screen = Screen()
 
 # Make screen listen
 screen.listen()
+
+# Forwards function
+def move_forward():
+    cursor.foward(10)
+
+# Backwards function
+def move_backwards():
+    cursor.backward(10)
